1b0d8e34507cc4d7782670bc76ee235645216003
yoonhyunwoo <yjs88zerg@gmail.com>
2022-03-04 21:43:08 +0900

    third commit

--- router/login.js
+++ router/login.js
@@ -5,7 +5,7 @@ const headCookieParser = require('../middleware/headCookieParser')
 
 router.get('/', (req,res)=>{
     if(req.headers.cookie == undefined){
-        res.sendFile('/workspace/token example/web/login.html'); // 토큰이 둘 다 없을 때는 로그인 페이지로 리다이렉트
+        res.sendFile('/workspace/Access-Refresh-Token-Example/web/login.html'); // 토큰이 둘 다 없을 때는 로그인 페이지로 리다이렉트
     }else{
     var ck = req.headers.cookie
     if(ck.includes(';')){
--- router/logout.js
+++ router/logout.js
@@ -3,7 +3,7 @@ const removeToken = require('../token/removeToken')
 
 
 router.get('/',(req,res)=>{
-    res.sendFile('/workspace/token example/web/logout.html');
+    res.sendFile('/workspace/Access-Refresh-Token-Example/web/logout.html');
 })
 
 router.use('/', removeToken)
